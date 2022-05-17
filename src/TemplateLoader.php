eaee28cbb064abadf978085543f1f37854c1d546
Lee Willis <lee@ademti-software.co.uk>
2022-05-17 09:51:23 +0100

    Fix optional parameter warning

--- src/TemplateLoader.php
+++ src/TemplateLoader.php
@@ -44,7 +44,7 @@ class TemplateLoader extends GamajoTemplateLoader {
 	 *
 	 * @return string             The rendered output.
 	 */
-	public function output( $slug, $name = null, $variables ) {
+	public function output( $slug, $name = null, $variables = array() ) {
 		echo $this->get( $slug, $name, $variables );
 	}
 }
